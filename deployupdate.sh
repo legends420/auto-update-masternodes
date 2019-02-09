b560c68cc0c76cd8d52ceabeb086170a9b5be680
legends420 <highlifesmokes@gmail.com>
2019-02-09 15:35:37 -0500

    Update deployupdate.sh

--- deployupdate.sh
+++ deployupdate.sh
@@ -13,27 +13,36 @@ function remove_olddaemons()
  cd ~/ganja > /dev/null 2>&1
  rm -r ganjacoind > /dev/null 2>&1
  rm -r ganjacoin-cli > /dev/null 2>&1
+ cd ~ > /dev/null 2>&1
  echo -e "done removing old daemons"
 }
 
 function download_daemons()
 {
  echo -e "downloading new clients & setting permissions"
+ cd ~/ganja > /dev/null 2>&1
  wget https://ganjacoinpro.com/downloads/ganjacoind > /dev/null 2>&1
  wget https://ganjacoinpro.com/downloads/ganjacoin-cli > /dev/null 2>&1
  chmod +x ganjacoin-cli
  chmod +x ganjacoind > /dev/null 2>&1
+ cd ~ > /dev/null 2>&1
+
  echo -e "done downloading & setting permissions"
 }
 
 function start_daemons()
 {
- ganjacoind > /dev/null 2>&1
+ cd ~/ganja > /dev/null 2>&1
+ ganjacoind -reindex > /dev/null 2>&1
+ cd ~ > /dev/null 2>&1
+
 }
 
 function check_daemons()
 {
+ cd ~/ganja > /dev/null 2>&1
  ganjacoin-cli getinfo
+
 }
 
 function deploy()
